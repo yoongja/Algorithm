04818de112c3f76610327fde382d20e74fc8a20c
yoongja <83260809+yoongja@users.noreply.github.com>
2021-08-29 00:00:44 +0900

    Create 문자열다루기기본.cpp

---
+++ programmers/문자열다루기기본.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+//#include <cctype>
+ 
+using namespace std;
+ 
+bool solution(string s) 
+{
+    //1. 길이로 쳐낸다.
+    const size_t length = s.length();
+    if(length != 4 && length != 6)
+    {
+        return false;
+    }
+ 
+    //2. 문자가 있으면 쳐낸다.
+    for(size_t i=0; i<length; ++i)
+    {
+        //if(!isdigit(s[i]))
+        if(!(s[i] >= '0' && s[i] <= '9'))
+        {
+            return false;
+        }
+    }
+ 
+    return true;
+}
