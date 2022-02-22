3d73944cae718fa1a7649357c191cbfc3f641923
= <rmatlr0112@naver.com>
2022-02-22 17:39:39 +0900

    2577'
    git commit -m 2577

---
+++ baekjoon/2577.py
@@ -0,0 +1,17 @@
+A = int(input())
+B = int(input())
+C = int(input())
+
+mul = A*B*C
+mul = str(mul)
+
+print(mul.count('0'))
+print(mul.count('1'))
+print(mul.count('2'))
+print(mul.count('3'))
+print(mul.count('4'))
+print(mul.count('5'))
+print(mul.count('6'))
+print(mul.count('7'))
+print(mul.count('8'))
+print(mul.count('9'))
