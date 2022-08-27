0027773b24e4bbdcf64ba7ea3c250a4a27241087
= <rmatlr0112@naver.com>
2022-08-27 14:42:02 +0900

    1018

--- baekjoon/1018.py
+++ baekjoon/1018.py
@@ -1,2 +1,13 @@
-N = list(map(int,input().split()))
-print(N)
+n,m = map(int,input().split())
+arr = [[] for _ in range(n)] #n행을 만듦
+
+for i in range(n):
+    for j in range(m): #m열을 받음
+        wb = input()
+        arr[i][j].append(wb)
+
+for _ in range(n):
+    if arr[0][0] == "W":
+        
+    if arr[0][0] == "B":
+        
